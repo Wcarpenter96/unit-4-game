4330317b73428c42f2f3d12e289f30c1d36cfa03
Weston Carpenter <wc.cc96@gmail.com>
2019-09-03 14:24:55 -0700

    Added game won/loss function, improved RPG interaction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,165 @@
 var selectedPlayer = false;
 var selectedEnemy = false;
+var enemiesDead = 0;
 
 var enemyHP
 var playerHP
 var attackPower
 
-var $Earth = $("#Earth")
+var $Water = $("#Water")
 var $Fire = $("#Fire")
 var $Air = $("#Air")
-var $Water = $("#Water")
+var $Earth = $("#Earth")
 
-$Earth.data({
-    health: 100,
-    attack: 10,
-    counter: 10
-});
-
-$Fire.data({
-    health: 50,
-    attack: 10,
-    counter: 10,
-});
-
-$Air.data({
-    health: 150,
-    attack: 10,
-    counter: 5,
-});
-
-$Water.data({
-    health: 100,
-    attack: 10,
-    counter: 5,
-});
-
+$('#Attack').attr('disabled', true);
 console.log('Select Player')
 
 $('.element').on('click', function () {
     if (!selectedPlayer) {
         $(this).addClass("offset-8 player")
         selectedPlayer = true
-        console.log('Select Enemy')
+        if ($Water.hasClass('player')) {
+            $Water.data({
+                health: 100,
+                attack: 10,
+                counter: 10,
+            });
+            $Fire.data({
+                health: 100,
+                attack: 10,
+                counter: 5,
+            });
+            $Air.data({
+                health: 50,
+                attack: 10,
+                counter: 5,
+            });
+            $Earth.data({
+                health: 150,
+                attack: 10,
+                counter: 15,
+            });
 
+        } else if ($Fire.hasClass('player')) {
+            $Water.data({
+                health: 150,
+                attack: 10,
+                counter: 15,
+            });
+            $Fire.data({
+                health: 100,
+                attack: 10,
+                counter: 10,
+            });
+            $Air.data({
+                health: 50,
+                attack: 10,
+                counter: 5,
+            });
+            $Earth.data({
+                health: 100,
+                attack: 10,
+                counter: 5,
+            });
+
+        } else if ($Air.hasClass('player')) {
+                $Water.data({
+                    health: 100,
+                    attack: 10,
+                    counter: 5,
+                });
+                $Fire.data({
+                    health: 50,
+                    attack: 10,
+                    counter: 15,
+                });
+                $Air.data({
+                    health: 100,
+                    attack: 10,
+                    counter: 10,
+                });
+                $Earth.data({
+                    health: 150,
+                    attack: 10,
+                    counter: 10,
+                });
+        } else {
+            $Water.data({
+                health: 50,
+                attack: 10,
+                counter: 15,
+            });
+            $Fire.data({
+                health: 150,
+                attack: 15,
+                counter: 10,
+            });
+            $Air.data({
+                health: 100,
+                attack: 10,
+                counter: 5,
+            });
+            $Earth.data({
+                health: 100,
+                attack: 10,
+                counter: 10,
+            });
+        }
+        playerHP = $('.player').data('health')
+        attackPower = $('.player').data('attack')
+        console.log('Select Enemy')
     }
     if (selectedPlayer && !selectedEnemy) {
         if (!$(this).hasClass("player")) {
             $(this).addClass("offset-4 enemy")
             selectedEnemy = true
             enemyHP = $('.enemy').data('health')
-            playerHP = $('.player').data('health')
-            attackPower = $('.player').data('attack')
+            $('#Attack').attr('disabled', false);
+            console.log(enemyHP)
         }
     }
 });
 
 $('#Attack').on('click', function () {
-
     enemyHP -= attackPower
-    playerHP -= $('.enemy').data('counter')
     attackPower += 2
+    if (enemyHP > 0) {
+        playerHP -= $('.enemy').data('counter')
+        if (playerHP <= 0) {
+            console.log('You lose')
+            GameOver();
+        }
+    }
+    else {
+        console.log('enemy dead')
+        $('#Attack').attr('disabled', true);
+        $('.enemy').hide('slow');
+        $('.element').removeClass('enemy')
+        enemiesDead++
+        if (enemiesDead == 3) {
+            console.log('You Win!')
+            GameOver();
+            if (playerHP == 0) {
+                console.log('*Special Acheivement: LAST STAND*')
+            }
+            if (playerHP == 30) {
+                console.log('*Special Acheivement: STRATEGIST*')
+            }
+        }
+        else {
+            selectedEnemy = false;
+            console.log('Select Another Enemy')
+        }
+    }
     console.log('Enemy HP: ' + enemyHP)
     console.log('Player HP: ' + playerHP)
 });
 
-
-$(document).on("click", ".player", function () {
-    console.log('player')
-});
-
-$(document).on("click", ".enemy", function () {
-    console.log('enemy')
-});
-
-
+function GameOver() {
+    $('#Attack').attr('disabled', true);
+    $('.element').removeClass('offset-4 offset-8 enemy player')
+    $('.element').show('slow');
+    selectedPlayer = false;
+    selectedEnemy = false;
+    enemiesDead = 0;
+}
